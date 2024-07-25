a521e0adda59297a7bea0d96e7dea3bddcb6612e
voncho <fencho.jay@gmail.com>
2024-07-25 18:35:34 +0800

    change the time zone to china

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -2,6 +2,25 @@ export function trimText(input: string, maxLength: number = 100): string {
   if (input.length <= maxLength) return input;
   return input.substring(0, maxLength - 3) + "...";
 }
+export function getCurrentTimeChina(): Date {
+  const now = new Date();
+  const offsetChina = 8;
+  now.setHours(now.getHours() + offsetChina);
+  return now;
+}
+export function formatTimeForChina(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+    timeZone: "Asia/Shanghai"
+  }
+  let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);
+  formattedTime += "CST";
+  return formattedTime;
+}
+
 export function getCurrentTimeInItaly(): Date {
   // Create a date object with the current UTC time
   const now = new Date();
