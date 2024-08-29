00b31e173d114420a684748ee3d064d295397787
voncho <fencho.jay@gmail.com>
2024-08-29 15:16:28 +0800

    fix: change personal name

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const LINKS = {
-  github: "https://github.com/coxvon",
+  github: "https://github.com/kidvon",
   // linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
   // medium: "https://ladvace.medium.com/",
   // discord: "https://discordapp.com/users/163300027618295808",
